b619ca9f3d48b90996683b719a12716fd6904cb5
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-09-03 16:21:32 +1000

    Created displayForecast function

--- src/app.js
+++ src/app.js
@@ -21,6 +21,36 @@ let day  = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+   let forecastHTML = `<div class="row">`;
+   let days = ["Fri", "Sat", "Sun", "Mon", "Tues"];
+   days.forEach(function (day) {
+     forecastHTML =
+       forecastHTML +
+       `
+      <div class="col-2">
+            <div class="weather-forecast-date">
+            ${day}
+            </div>
+            <img src="https://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperatures">           
+            <span class="weather-forecast-temperature-max">
+            18° </span>
+            <span class="weather-forecast-temperature-min">
+            12° </span>
+            </div>
+        </div>
+       `;
+   });
+
+       forecastHTML = forecastHTML+ `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+} 
 
 function displayTemperature(response) {
 let temperature = document.querySelector("#temperature");
@@ -86,3 +116,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("New York");
+displayForecast();
