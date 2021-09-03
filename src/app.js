25279cb3c56cbc2b0efc6e27c9f337ed591da603
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-09-03 21:32:14 +1000

    Removed the fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -94,8 +94,6 @@ getForecast(response.data.coord);
 
 }
 
-
-
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
@@ -107,31 +105,6 @@ let apiKey = "9ed18fb70d0b270f7a828de481fab593";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 }
-
-function showFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperature = document.querySelector("#temperature");
-    celciusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (14 * 9) / 5 + 32;
-    temperature.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function showcelciusTemperature(event) {
-    event.preventDefault();
-    let temperature = document.querySelector("#temperature");
-      celciusLink.classList.add("active");
-      fahrenheitLink.classList.remove("active");
-    temperature.innerHTML = Math.round(celciusTemperature);
-}
-
-let celciusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
-
-let celciusLink = document.querySelector("#celcius-link");
-celciusLink.addEventListener("click", showcelciusTemperature);
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
